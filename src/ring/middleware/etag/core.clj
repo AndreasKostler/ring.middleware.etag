d617ac0c8205115fc3164609eaeab26630646b39
= <=>
2011-11-20 15:36:23 +1030

    Dispatch only on first arg.

--- src/ring/middleware/etag/core.clj
+++ src/ring/middleware/etag/core.clj
@@ -2,20 +2,20 @@
   (:import [org.apache.commons.codec.digest DigestUtils]))
 
 ;; A bunch of useful hashing/uuid etag generators.
-(defn- md5 [s]
+(defn md5 [s]
   (DigestUtils/md5Hex s))
-(defn- sha [s]
+(defn sha [s]
   (DigestUtils/shaHex s))
-(defn- sha256 [s]
+(defn sha256 [s]
   (DigestUtils/sha256Hex s))
-(defn- sha384 [s]
+(defn sha384 [s]
   (DigestUtils/sha384Hex s))
-(defn- sha512 [s]
+(defn sha512 [s]
   (DigestUtils/sha512Hex s))
-(defn- uid []
+(defn uid []
   (.toString (java.util.UUID/randomUUID)))
 
-(defmulti calculate-etag class)
+(defmulti calculate-etag (fn [x _] (class x)))
 
 (defmethod calculate-etag String [s fun] (fun s))
 (defmethod calculate-etag java.io.File
@@ -32,6 +32,13 @@
 (defn- not-modified-response [etag]
   {:status 304 :body "" :headers {"etag" etag}})
 
+(defn- cached-response
+  "Attach an etag header to response header. If old-etag and new-etag match then return a 304."
+  [old-etag new-etag response]
+  (if (= old-etag new-etag)
+    (not-modified-response new-etag)
+    (assoc-in response [:headers "etag"] new-etag)))
+
 (defn with-etag
   "Generates an etag header for a response body according to etag-generator and transforms response according to response-fn."
   [handler {:keys [etag-generator response-fn]
@@ -42,13 +49,6 @@
           response (handler request)
           new-etag (etag-generator response)]
       (response-fn old-etag new-etag response))))
-
-(defn- cached-response
-  "Attach an etag header to response header. If old-etag and new-etag match then return a 304."
-  [old-etag new-etag response]
-  (if (= old-etag new-etag)
-    (not-modified-response new-etag)
-    (assoc-in response [:headers "etag"] new-etag)))
 
 (defn with-etag-cache [handler]
   (with-etag handler {:etag-generator (create-hashed-etag-fn md5) :response-fn cached-response}))
