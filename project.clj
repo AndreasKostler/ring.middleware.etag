5a8dbea076f6b76d71065f4ab5589d3b2e77a506
= <=>
2011-11-20 15:47:44 +1030

    Removed clojure-contrib.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject ring.middleware.etag "1.0.0-SNAPSHOT"
   :description "ETag middleware for ring"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [commons-codec/commons-codec "1.5"]])
