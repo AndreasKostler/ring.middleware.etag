ed2b0daa91c5589bf76d96b12b944a730aca287f
Andreas Koestler <andreas@Andreass-MacBook-Pro.local>
2014-06-02 21:19:42 +0930

    Cleanup

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject ring.middleware.etag "1.0.0-SNAPSHOT"
+(defproject ring.middleware.etag "1.0.1"
   :description "ETag middleware for ring"
-  :dependencies [[commons-codec/commons-codec "1.5"]])
+  :url "https://github.com/AndreasKostler/ring.middleware.etag"
+  :author "Andreas Koestler"
+  :license "MIT"
+  :dependencies [[org.clojure/clojure "1.6.0"]])
--- src/ring/middleware/etag/core.clj
+++ src/ring/middleware/etag/core.clj
@@ -1,36 +1,10 @@
-(ns ring.middleware.etag.core
-  (:import [org.apache.commons.codec.digest DigestUtils]))
-
-;; A bunch of useful hashing/uuid etag generators.
-(defn md5 [s]
-  (DigestUtils/md5Hex s))
-(defn sha [s]
-  (DigestUtils/shaHex s))
-(defn sha256 [s]
-  (DigestUtils/sha256Hex s))
-(defn sha384 [s]
-  (DigestUtils/sha384Hex s))
-(defn sha512 [s]
-  (DigestUtils/sha512Hex s))
-(defn uid []
-  (.toString (java.util.UUID/randomUUID)))
-
-(defmulti calculate-etag (fn [x _] (class x)))
-
-(defmethod calculate-etag String [s fun] (fun s))
-(defmethod calculate-etag java.io.File
-  [f fun]
-  (calculate-etag (slurp f) fun))
-
-(defn create-hashed-etag-fn [hash-fn]
-  (fn [response]
-    (calculate-etag (:body response) hash-fn)))
-
-(defn create-uid [_]
-  (uid))
+(ns ring.middleware.etag.core)
 
 (defn- not-modified-response [etag]
   {:status 304 :body "" :headers {"etag" etag}})
+
+(defn- uid []
+  (.toString (java.util.UUID/randomUUID)))
 
 (defn- cached-response
   "Attach an etag header to response header. If old-etag and new-etag match then return a 304."
@@ -42,7 +16,7 @@
 (defn with-etag
   "Generates an etag header for a response body according to etag-generator and transforms response according to response-fn."
   [handler {:keys [etag-generator response-fn]
-            :or {etag-generator create-uid
+            :or {etag-generator #(uid)
                  response-fn cached-response}}]
   (fn [request]
     (let [old-etag (get-in request [:headers "if-none-match"])
@@ -50,5 +24,7 @@
           new-etag (etag-generator response)]
       (response-fn old-etag new-etag response))))
 
-(defn with-etag-cache [handler]
-  (with-etag handler {:etag-generator (create-hashed-etag-fn md5) :response-fn cached-response}))
+(defn with-etag-cache [handler hash-fn]
+  (with-etag handler
+    {:etag-generator (fn [respone] (hash-fn (:body response)))
+     :response-fn cached-response}))
